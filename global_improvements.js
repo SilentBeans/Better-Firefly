b7b8561a11e8b24834278c77e04ff66cd629b099
Finley Cooper <contact@finleycooper.co.uk>
2021-02-26 08:40:39 +0000

    Added Better Firefly Help link to global

--- global_improvements.js
+++ global_improvements.js
@@ -10,6 +10,26 @@ chrome.storage.local.get(['dark_mode', 'pfp_status', 'name'], function(result) {
     };
 });
 
+//change Personal Pages to Better Firefly
+document.querySelector("#icon-personal-pages").remove();
+logoTag = document.createElement("img");
+logosrc = document.createAttribute("src");
+logoWidth = document.createAttribute("width");
+logoHeight = document.createAttribute("height");
+logosrc.value = "https://finleycooper.co.uk/static/images/better-firefly/white-logo.png";
+logoWidth.value = "24";
+logoHeight.value = "24";
+logoTag.setAttributeNode(logosrc);
+logoTag.setAttributeNode(logoHeight);
+logoTag.setAttributeNode(logoWidth);
+document.querySelector("#react-navigation > div > div > div > div > div.css-2e6eiu > div:nth-child(5) > a > div").appendChild(logoTag);
+
+document.querySelector("#react-navigation > div > div > div > div > div.css-2e6eiu > div:nth-child(5) > a > span").innerHTML = "Better Firefly Help";
+
+var helpLink = document.querySelector("#react-navigation > div > div > div > div > div.css-2e6eiu > div:nth-child(5) > a");
+helpLink.href = "https://finleycooper.co.uk/better-firefly/help";
+helpLink.setAttribute("target", "_blank")
+helpLink.setAttribute("rel", "noopener noreferrer")
 
 function setDarkmode() {
     document.body.style.backgroundColor = "#323232";// MAIN BACKGROUND
